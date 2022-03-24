50760708b28e5ea5a6106cf49c97d496475a7fa4
mhassan111 <mhassanuetcs12@gmail.com>
2022-03-24 18:57:52 +0500

    first

---
+++ app/src/main/java/com/ktor/api/data/remote/PostServiceImpl.kt
@@ -0,0 +1,57 @@
+package com.ktor.api.data.remote
+
+import com.ktor.api.data.remote.dto.PostRequest
+import com.ktor.api.data.remote.dto.PostResponse
+import io.ktor.client.*
+import io.ktor.client.features.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+
+class PostServiceImpl(private val client: HttpClient) : PostService {
+
+    override suspend fun getPosts(): List<PostResponse> {
+        return try {
+            client.get { url(HttpRoutes.POSTS) }
+        } catch (e: RedirectResponseException) {
+            // 3xx - responses
+            println("Error: ${e.response.status.description}")
+            emptyList()
+        } catch (e: ClientRequestException) {
+            // 4xx - responses
+            println("Error: ${e.response.status.description}")
+            emptyList()
+        } catch (e: ServerResponseException) {
+            // 5xx - responses
+            println("Error: ${e.response.status.description}")
+            emptyList()
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+            emptyList()
+        }
+    }
+
+    override suspend fun createPost(postRequest: PostRequest): PostResponse? {
+        return try {
+            client.post<PostResponse> {
+                url(HttpRoutes.POSTS)
+                contentType(ContentType.Application.Json)
+                body = postRequest
+            }
+        } catch (e: RedirectResponseException) {
+            // 3xx - responses
+            println("Error: ${e.response.status.description}")
+            null
+        } catch (e: ClientRequestException) {
+            // 4xx - responses
+            println("Error: ${e.response.status.description}")
+            null
+        } catch (e: ServerResponseException) {
+            // 5xx - responses
+            println("Error: ${e.response.status.description}")
+            null
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+            null
+        }
+    }
+}
